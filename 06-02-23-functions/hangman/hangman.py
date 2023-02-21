d4792a65fc38419f1d2dec362f735d637e3ba9de
Gijs ter Haar <gijs.terhaar@fox-it.com>
2023-02-21 17:51:30 +0100

    my second version with the correct no-count-down when guess is True

--- 06-02-23-functions/hangman/hangman.py
+++ 06-02-23-functions/hangman/hangman.py
@@ -15,7 +15,6 @@ def get_a_word():
     for words in word_file:
         word_list.append(words.strip().lower())
     word = random.choice(word_list)
-    print(word)
     return list(word)
 
 def welcome():
@@ -30,30 +29,33 @@ def first_compare(word, your_guess, answer_line, count):
         if your_guess == char:
             answer_line.pop(i)
             answer_line.insert(i, your_guess)
+    if answer_line == ['.', '.', '.', '.', '.', '.']:
+        count -= 1
     first_print_and_into_the_loop(word, answer_line, count)
 
 def first_print_and_into_the_loop(word, answer_line, count):
-    while answer_line != word and count > 1:
+    while answer_line != word and count > 0:
         answer_line = ''.join(answer_line)
         print(answer_line)
         count, word, answer_line = and_loop_again(count, word, answer_line)
     stop(word, answer_line)
 
 def and_loop_again(count, word, answer_line):
-    count -= 1
-    your_guess = guess()
     answer_line = list(answer_line)
-    check_looping(word, answer_line, your_guess)
+    
+    your_guess = guess()
+    count = check_looping(count, word, answer_line, your_guess)
     return count, word, answer_line
 
-def check_looping(word, answer_line, your_guess):
+def check_looping(count, word, answer_line, your_guess):
+    if your_guess not in word:
+        count -= 1
     for i, char in enumerate(word):
         if your_guess == char:
             answer_line.pop(i)
             answer_line.insert(i, your_guess)
-    return word, answer_line, your_guess
+    return count
     
-
 def stop(word, answer_line):
     if word == answer_line:
         answer_line = ''.join(answer_line)
