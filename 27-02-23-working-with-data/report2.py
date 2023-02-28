ece858fbb6990a13b8ffe7009bdce1c0778ebd40
Gijs ter Haar <gijs.terhaar@fox-it.com>
2023-02-28 13:01:49 +0100

    Sorry mate, had one more epiphany and put the variables count, highest and lowest in the functions in main as a keyword-argument

--- 27-02-23-working-with-data/report2.py
+++ 27-02-23-working-with-data/report2.py
@@ -3,34 +3,31 @@ import movie_data
 def main():
     movie_library = movie_data.movies
     total_movies(movie_library)
-    average_rating(movie_library)
-    best_movie(movie_library)
-    worst_movie(movie_library)
+    average_rating(movie_library, count=0)
+    best_movie(movie_library, highest=0)
+    worst_movie(movie_library, lowest=11)
 
 def total_movies(movie_library):
     print(f'\nThere are {len(movie_library)} movies in this folder')
 
-def average_rating(movie_library):
-    count = 0
+def average_rating(movie_library, count):
     for movie in movie_library:
         count += movie.get('rating')
     print(f'\nThe average rating is {count/len(movie_library)}')
 
-def best_movie(movie_library):
-    highest = 0
+def best_movie(movie_library, highest):
     for movie in movie_library:
         if movie.get('rating') > highest:
             highest = movie.get('rating')
             best = movie.get('title')
-    print(f'\nThe best movie is {best} with a rating of {highest}')
+    print(f'\nThe best movie is {best}, with a rating of {highest}')
 
-def worst_movie(movie_library):
-    lowest = 11
+def worst_movie(movie_library, lowest):
     for movie in movie_library:
         if movie.get('rating') < lowest:
             lowest = movie.get('rating')
             worst = movie.get('title')
-    print(f'\nThe worst movie is {worst} with a rating of {lowest}\n')
+    print(f'\nThe worst movie is {worst}, with a rating of {lowest}\n')
 
 if __name__ == '__main__':
     main()
