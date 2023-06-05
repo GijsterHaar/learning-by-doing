1998eb08ccecdf690e7720face8238054235c20e
Gijs ter Haar <gijs.terhaar@fox-it.com>
2023-06-05 19:16:43 +0200

    Rummy_py: extended

--- 07-assessment-2/rummy.py
+++ 07-assessment-2/rummy.py
@@ -2,6 +2,7 @@ from pure_rummy import description_rummy_hand
 
 def main():
     user_input = input("What's your hand? ")
+
     rummy_hand = description_rummy_hand(user_input)
     print(rummy_hand)
 
--- 07-assessment-2/test_pure_rummy.py
+++ 07-assessment-2/test_pure_rummy.py
@@ -1,5 +1,5 @@
 from pure_rummy import description_rummy_hand
-INVALID_STRING = 'Invalid Input'
+INVALID_STRING = 'INVALID'
 
 def test_find_function():
     assert description_rummy_hand
@@ -19,50 +19,44 @@ def test_invalid_input_list():
 def test_invalid_input_wrong_numbers_of_cards():
     compare_input_to_expected_result("AS")
 
-def test_three_of_a_kind_ranks():
-    compare_input_to_expected_result("AC AS 5C 10C AH 2H 5H", "1 Three of a kind, you lose")
+def test_1_three_of_a_kind_ranks():
+    compare_input_to_expected_result("AC AS 5C 10C AH 2H 5H", "LOSE")
 
-def test_three_of_a_kind_ranks_and_four_others_all_different():
-    compare_input_to_expected_result("AC AS 5C 10C AH 2H 3H", "1 Three of a kind, you lose")
+def test_1_three_of_a_kind_ranks_and_four_others_all_different():
+    compare_input_to_expected_result("AC AS 5C 10C AH 2H 3H", "LOSE")
 
 def test_invalid_input_with_at_least_twenty_characters_but_10_in_it_and_one_space_missing():
     compare_input_to_expected_result("10C10S 5C 10C AH 2H 5H")
 
 def test_valid_input_ranks_with_to_manny_spaces_in_between():
-    compare_input_to_expected_result("10C    10S 5C   10C AH 2H 5H", "1 Three of a kind, you lose")
+    compare_input_to_expected_result("10C    10S 5C   10C AH 2H 5H", "LOSE")
 
 def test_invalid_input_with_eight_cards():
     compare_input_to_expected_result("AC AS 5C 10C AH 2H 3H 6H")
 
 def test_invalid_input_lower():
-    compare_input_to_expected_result("ac as 5c 10c 2h 5h ah", "1 Three of a kind, you lose")
+    compare_input_to_expected_result("ac as 5c 10c 2h 5h ah", "LOSE")
 
 def test_four_of_a_kind_ranks():
-    compare_input_to_expected_result("AC AS 5C 10S AH AD 5H", "1 Four of a kind, you lose")
+    compare_input_to_expected_result("AC AS 5C 10S AH AD 5H", "LOSE")
 
 def test_four_of_a_kind_ranks_and_three_others_all_different():
-    compare_input_to_expected_result("AC AS 5C 10S AH AD 3H", "1 Four of a kind, you lose")
+    compare_input_to_expected_result("AC AS 5C 10S AH AD 3H", "LOSE")
 
 def test_three_and_four_of_a_kind_ranks():
-    compare_input_to_expected_result("AC AS 10S 10C AH AD 10H", "1 Three of a kind and 1 Four of a kind, you win")
+    compare_input_to_expected_result("KC KS 10S 10C KH KD 10H", "WIN")
 
-def test_invalid_input_with_unknown_suit():
-    compare_input_to_expected_result("AC AS 5C 10B AH 2H 5H")
+def test_three_and_four_of_a_kind_ranks_with_aces():
+    compare_input_to_expected_result("AC AS 10S 10C AH AD 10H", "WIN")
 
-def test_three_of_a_kind_suits():
-    compare_input_to_expected_result("2C 4C JC 4D 7D 3H QH", "1 Three of a kind, you lose")
+def test_1_straight_three_cards():
+    compare_input_to_expected_result("AH 2H 3H 4C 5S 6D 7H", "LOSE")
 
-def test_four_of_a_kind_suits():
-    compare_input_to_expected_result("2C 4C JC 4D 7C 3H QH", "1 Four of a kind, you lose")
+def test_1_straight_four_cards():
+    compare_input_to_expected_result("AH 2H 3H 4H 5S 6D 7H", "LOSE")
 
-def test_three_and_four_of_kind_suits():
-    compare_input_to_expected_result("2C 4C JC 4D 7C 3D QD", "1 Three of a kind and 1 Four of a kind, you win")
-
-def test_three_of_a_kind_ranks_and_four_of_a_kind_suits():
-    compare_input_to_expected_result("2C 2D 2H 4C 6C 7C 8H", "1 Three of a kind and 1 Four of a kind, you win")
-
-def test_four_of_a_kind_ranks_and_three_of_a_kind_suits():
-    compare_input_to_expected_result("2C 2D 2H 2S 6H 7C 8H", "1 Three of a kind and 1 Four of a kind, you win")
+# def test_straight_four_and_three_together():
+#     compare_input_to_expected_result("AH 2H 3H 4H 5S 6S 7S", "WIN")
 
 
 def compare_input_to_expected_result(user_input, rummy_hand=INVALID_STRING):
