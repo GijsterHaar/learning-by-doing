0de4f799e924bbd47d70ef9e6c2b5e6250f43e21
Gijs ter Haar <gijs.terhaar@fox-it.com>
2023-06-26 17:56:39 +0200

    some changes to assesment 07 and first poker OOP

--- 07-assessment-2/pure_rummy.py
+++ 07-assessment-2/pure_rummy.py
@@ -1,7 +1,7 @@
 
 def description_rummy_hand(user_input):
     suits = ['H', 'C', 'D', 'S']
-    rank_order_straight = ['A', '2', '3', '4', '5', '6', '7',
+    rank_order_straight = ['1', '2', '3', '4', '5', '6', '7',
                     '8', '9', '10', 'J', 'Q', 'K']
     # chunk_size = [3, 4]
     # for rank in rank_order_straight:
@@ -11,20 +11,29 @@ def description_rummy_hand(user_input):
     #     chunked_ranks_four.sort()
 
     if not isinstance(user_input, str) or user_input == '' or len(user_input) < 20:
-        return 'Invalid Input'
+        return 'INVALID'
     user_input = user_input.upper()
 
     frequency_ranks = {}
     ranks_list = [i[0:-1] for i in user_input.split()]
     if not len(ranks_list) == 7:
-        return 'Invalid Input'
+        return 'INVALID'
     ranks_list.sort()
+    frequency_ranks = {}
+    ranks_list = [i[0:-1] for i in user_input.split()]
+    if not len(ranks_list) == 7:
+        return 'INVALID'
+    ranks_list.sort()
+    for _ in range(7):
+        if ranks_list[-1] == "A":
+            ranks_list[-1] = '1'
+        ranks_list.sort()
 
     frequency_suits = {}
     suit_list = [i[-1] for i in user_input.split()]
     for suit in suit_list:
         if not suit in suits:
-            return 'Invalid Input'
+            return 'INVALID'
 
     for rank in ranks_list:
         if rank not in frequency_ranks:
@@ -37,20 +46,7 @@ def description_rummy_hand(user_input):
         frequency_suits[suit] += 1
 
     if len(frequency_ranks) == 2 and 4 in frequency_ranks.values():
-        return "1 Three of a kind and 1 Four of a kind, you win"
-    elif len(frequency_suits) == 2 and 4 in frequency_suits.values():
-        return "1 Three of a kind and 1 Four of a kind, you win"
-    elif 3 in frequency_ranks.values() and 4 in frequency_suits.values():
-        return "1 Three of a kind and 1 Four of a kind, you win"
-    elif 4 in frequency_ranks.values() and 3 in frequency_suits.values():
-        return "1 Three of a kind and 1 Four of a kind, you win"
-    elif len(frequency_ranks) < 6 and 3 in frequency_ranks.values():
-        return "1 Three of a kind, you lose"
-    elif len(frequency_ranks) < 5 and 4 in frequency_ranks.values():
-        return "1 Four of a kind, you lose"
-    elif len(frequency_suits) < 6 and 3 in frequency_suits.values():
-        return "1 Three of a kind, you lose"
-    elif len(frequency_suits) < 5 and 4 in frequency_suits.values():
-        return "1 Four of a kind, you lose"
+        return "WIN"
+    return "LOSE"
 
-    print(frequency_ranks)
+
