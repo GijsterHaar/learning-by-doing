103cc813a38fcd5872e86fd5b7e5053bba7bc781
Gijs ter Haar <gijs.terhaar@fox-it.com>
2023-09-19 12:29:58 +0200

    My counter class

--- 09-and-a-half-counters/counter.py
+++ 09-and-a-half-counters/counter.py
@@ -1,5 +1,21 @@
 class Counter:
-    pass
+    
+    def __init__(self):
+        self.counter = 0
+        
+    def add(self):
+        self.counter += 1
+        return self.counter
+    
+    def total(self):
+        return self.counter
+    
+    def reset(self):
+        self.counter = 0
+        return self.counter
+
+
+
 
 # CARLA BOUNDARY
 # Fill in the blanks in the Counter class to make
@@ -14,24 +30,24 @@ pies_eaten.add()
 # You have eaten 2 pies
 print(f'You have eaten {pies_eaten.total()} pies')
 pies_eaten.add()
-# You have eaten 3 pies
+# # You have eaten 3 pies
 print(f'You have eaten {pies_eaten.total()} pies')
 
 days_since_accident = Counter()
-# It has been 0 days since an accident
+# # It has been 0 days since an accident
 print(f'It has been {days_since_accident.total()} days since an accident')
 for i in range(10):
     days_since_accident.add()
-# It has been 10 days since an accident
+# # It has been 10 days since an accident
 print(f'It has been {days_since_accident.total()} days since an accident')
-# Goddamnit, Irene
+# # Goddamnit, Irene
 print('Oh no! Due to Irene running in a no running zone, Colin has lost both legs!')
 days_since_accident.reset()
-# It has been 0 days since an accident
+# # It has been 0 days since an accident
 print(f'It has been {days_since_accident.total()} days since an accident')
 
 pies_eaten.add()
-# You have eaten 4 pies
+# # You have eaten 4 pies
 print(f'You have eaten {pies_eaten.total()} pies')
 
 
