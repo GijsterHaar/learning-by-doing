28eae0a0d82d18ba3f69863ae233c83871eb57c0
Gijs ter Haar <gijs.terhaar@fox-it.com>
2023-10-30 15:27:47 +0100

    All refactored and all tests written

--- bitbucket-demo/bitbucket_demo.py
+++ bitbucket-demo/bitbucket_demo.py
@@ -3,10 +3,14 @@ print()
 
 def main():
     arguments = {'limit': 15}
+    # we call the get obj function with the extension of the path into the customer repos
     repo_obj = get_obj("/api/latest/projects/CUSTOMER/repos", arguments)
     repositories = get_repositories(repo_obj)
     name_committer = input("Please enter the name you want to search for in the commits: ")
 
+    # line 11 - 23 both use the get_commit_list function to first:
+    # - get commits by name per repository
+    # - get average time for commits per repository
     print()
     for repository in repositories:
         commit_list = get_commit_list(repository)
@@ -19,21 +23,23 @@ def main():
         average_time = get_average_time_for_commits_per_repository(commit_list, repository)        
         print(f"{average_time}")
         
-        
+
 URL = "https://bitbucket.detact.fox.local/rest"
 HEADERS = {'Authorization': 'Bearer Njc0MDE4NTkzMTg2OtA13694w2W0nl1ppGTm/O4C+Uc6'}
 
-
+# get the customer repo object, it takes the URL en HEADERS from the constants
+# the path en arguments from trhe function call
 def get_obj(path, arguments):
     r = requests.get(URL+path, arguments, headers=HEADERS)
     return r.json()
 
-
+# this function only gets a list with all the names of the repositories
+# to itterate over to extract data per or for all repos
 def get_repositories(repo_obj):
     repositories = [i['name'] for i in repo_obj['values']]
     return repositories
 
-
+# 
 def get_commit_list(repository):
     arguments = {'limit': 100}
     path = f"/api/latest/projects/CUSTOMER/repos/{repository}/commits"
@@ -56,11 +62,10 @@ def get_commits_by_name_per_repository(commit_list, repository, name_committer):
 # this function deducts the author timestamp (when ticket was created) from committer timestamp (time of commit)
 # I then divide by 1000 to get rid of milliseconds, by 60 to turn seconds into minutes, and by 60 to turn minutes into hours
 def get_average_time_for_commits_per_repository(commit_list, repository, ):
-    time_spend_list = [(commit['committerTimestamp'] - commit['authorTimestamp']) for commit in commit_list \
+    time_spend_list = [(commit['committerTimestamp'] - commit['authorTimestamp']) // 1000 // 60 / 60 for commit in commit_list \
                        if (commit['committerTimestamp'] - commit['authorTimestamp']) != 0]
     average_time = sum(time_spend_list) / len(time_spend_list)
-    return f"the average time for commits in the {repository} repository is {average_time // 1000 // 60 // 60} hours"
-
+    return f"the average time for commits in the {repository} repository is {average_time: .2f} hours"
 
 if __name__ == '__main__':
     main()
--- bitbucket-demo/test_bitbucket_demo.py
+++ bitbucket-demo/test_bitbucket_demo.py
@@ -1,5 +1,7 @@
-import pytest
-from bitbucket_demo  import get_repositories, get_commits_by_name_per_repository
+
+from bitbucket_demo  import get_repositories, get_commits_by_name_per_repository,\
+get_average_time_for_commits_per_repository
+
 
 def test_get_the_repositories():
     data = {'values': [{'name': 'gijs'}, {'name': 'captain jock strap'}], 'irrelevant': "ignore me"}
@@ -8,13 +10,32 @@ def test_get_the_repositories():
 
 
 def test_get_the_commits_by_name_per_repository():
-    data = {'values': [{"author": {'name': 'tim.vandermeij'}}], 'irrelevant': 'ignore me'}
+    data = [{"author": {'name': 'tim.vandermeij'}}]
     repository = 'blue'
-    result = get_commits_by_name_per_repository(data, repository)
+    name_commiter = 'tim.vandermeij'
+    result = get_commits_by_name_per_repository(data, repository, name_commiter)
     assert result == "tim.vandermeij has contributed 1 commits to the blue repository"
 
+
 def test_get_the_commits_by_name_per_repository_failed():
-    data = {'values': [], 'irrelevant': 'ignore me'}
+    data = []
     repository = 'blue'
     result = get_commits_by_name_per_repository(data, repository, 'tim.vandermeij')
     assert result == "tim.vandermeij contributed no commits to the blue repository"
+
+
+def test_get_average_time_for_commits_per_repository():
+    data = [{'committerTimestamp': 1679585391000, 'authorTimestamp': 1679571522000} ,
+            {'committerTimestamp': 1679586391000, 'authorTimestamp': 1679571522000}]
+    repository = 'blue'
+    result = get_average_time_for_commits_per_repository(data, repository)
+    assert result == 'the average time for commits in the blue repository is  3.98 hours'
+
+
+def test_get_average_time_for_commits_per_repository_with_a_zero_as_sum_deduction():
+    data = [{'committerTimestamp': 1679585391000, 'authorTimestamp': 1679571522000},
+            {'committerTimestamp': 1679586391000, 'authorTimestamp': 1679571522000},
+            {'committerTimestamp': 1679586391000, 'authorTimestamp': 1679586391000}] # these deduct to zero
+    repository = 'blue'
+    result = get_average_time_for_commits_per_repository(data, repository)
+    assert result == 'the average time for commits in the blue repository is  3.98 hours'
